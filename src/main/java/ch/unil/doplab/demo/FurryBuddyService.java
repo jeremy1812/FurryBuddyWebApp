e78bb98a0b14f002d47cbccff70c4364e607c32e
jeremy <jeremy.furlan@unil.ch>
2024-11-21 11:56:33 +0100

    later adjustement

--- src/main/java/ch/unil/doplab/demo/FurryBuddyService.java
+++ src/main/java/ch/unil/doplab/demo/FurryBuddyService.java
@@ -1,6 +1,8 @@
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")  // à la place de /api on va mettre le chemin où FurryBuddyService est deployé (exemple : http://localhost:8080)
+@ApplicationPath("http://localhost:8080/FurryBuddyWebApp/api")  // à la place de /api on va mettre le chemin où FurryBuddyService est deployé (exemple : http://localhost:8080)
 public class FurryBuddyService extends Application {
 }
+
+http://localhost:8080/FurryBuddyWebApp/api
