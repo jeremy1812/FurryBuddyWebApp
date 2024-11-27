179d50a64eb50a2264ce91e4575fb9eb496e52f4
audreymartelly <73237246+audreymartelly@users.noreply.github.com>
2024-11-27 21:35:25 +0100

    add service calls for all classes

--- src/main/java/ch/unil/doplab/demo/FurryBuddyService.java
+++ src/main/java/ch/unil/doplab/demo/FurryBuddyService.java
@@ -1,9 +1,6 @@
 package ch.unil.doplab.demo;
 
-import ch.unil.furrybuddy.domain.Adopter;
-import ch.unil.furrybuddy.domain.Advertisement;
-import ch.unil.furrybuddy.domain.ExceptionDescription;
-import ch.unil.furrybuddy.domain.PetOwner;
+import ch.unil.furrybuddy.domain.*;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.client.Client;
@@ -49,8 +46,19 @@ public class FurryBuddyService {
                 .get(String.class);
     }
 
+    public UUID authenticate(String email, String password, String role) {
+        var response = serviceTarget
+                .path("authenticate")
+                .path(email)
+                .path(password)
+                .path(role)
+                .request(MediaType.APPLICATION_JSON)
+                .get(UUID.class);
+        return response;
+    }
+
     /*
-     * PetOwner operations
+     PetOwner operations
      */
 
     public PetOwner getPetOwner(String id) {
@@ -60,6 +68,7 @@ public class FurryBuddyService {
                 .get(PetOwner.class);
         return petowner;
     }
+
     public PetOwner addPetOwner(PetOwner petOwner) throws Exception {
         petOwner.setUserID(null);  // Ensure ID is not set to avoid server-side UUID errors
         Response response = petOwnerTarget
@@ -80,6 +89,56 @@ public class FurryBuddyService {
             }
         }
     }
+
+    public boolean setPetOwner(PetOwner petOwner) throws Exception {
+        var response = petOwnerTarget
+                .path(petOwner.getUserID().toString())
+                .request(MediaType.APPLICATION_JSON)
+                .put(Entity.entity(petOwner, MediaType.APPLICATION_JSON));
+        if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+            return response.getStatus() == 200;
+        } else {
+            ExceptionDescription description = response.readEntity(ExceptionDescription.class);
+//            throw Utils.buildException(description); //TODO
+            try {
+                Class<?> exceptionClass = Class.forName(description.getType());
+                Constructor<?> constructor = exceptionClass.getConstructor(String.class);
+                Exception exception = (Exception) constructor.newInstance(description.getMessage());
+                throw exception;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public List<PetOwner> getAllPetOwners() {
+        var petOwners = petOwnerTarget
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<PetOwner>>() {
+                });
+        return petOwners;
+    }
+
+    public boolean deletePetOwner(UUID petOwnerID) {
+        var response = petOwnerTarget
+                .path(petOwnerID.toString())
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+        return response.getStatus() == 200;
+    }
+
+    /*
+     Adopter operations
+     */
+
+    public Adopter getAdopter(String id) {
+        var adopter = adopterTarget
+                .path(id.toString())
+                .request()
+                .get(Adopter.class);
+        return adopter;
+    }
+
     public Adopter addAdopter(Adopter adopter) throws Exception {
         adopter.setUserID(null);  // Ensure ID is not set to avoid server-side UUID errors
         Response response = adopterTarget
@@ -99,6 +158,43 @@ public class FurryBuddyService {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public boolean setAdopter(Adopter adopter) throws Exception {
+        var response = adopterTarget
+                .path(adopter.getUserID().toString())
+                .request(MediaType.APPLICATION_JSON)
+                .put(Entity.entity(adopter, MediaType.APPLICATION_JSON));
+        if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+            return response.getStatus() == 200;
+        } else {
+            ExceptionDescription description = response.readEntity(ExceptionDescription.class);
+//            throw Utils.buildException(description); //TODO
+            try {
+                Class<?> exceptionClass = Class.forName(description.getType());
+                Constructor<?> constructor = exceptionClass.getConstructor(String.class);
+                Exception exception = (Exception) constructor.newInstance(description.getMessage());
+                throw exception;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public List<Adopter> getAllAdopters() {
+        var adopters = adopterTarget
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<Adopter>>() {
+                });
+        return adopters;
+    }
+
+    public boolean deleteAdopter(UUID adopterID) {
+        var response = adopterTarget
+                .path(adopterID.toString())
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+        return response.getStatus() == 200;
     }
 
     /*
@@ -173,4 +269,79 @@ public class FurryBuddyService {
                 .delete();
         return response.getStatus() == 200;
     }
+
+    /*
+    Adoption request operations
+     */
+    public AdoptionRequest getAdoptionRequest(UUID requestID) {
+        var adoptionRequest = adoptionRequestTarget
+                .path(requestID.toString())
+                .request()
+                .get(AdoptionRequest.class);
+        return adoptionRequest;
+    }
+
+    public boolean setAdoptionRequest(AdoptionRequest adoptionRequest) throws Exception {
+        var response = advertisementTarget
+                .path(adoptionRequest.getRequestID().toString())
+                .request(MediaType.APPLICATION_JSON)
+                .put(Entity.entity(adoptionRequest, MediaType.APPLICATION_JSON));
+        if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+            return response.getStatus() == 200;
+        } else {
+            ExceptionDescription description = response.readEntity(ExceptionDescription.class);
+//            throw Utils.buildException(description); //TODO
+            try {
+                Class<?> exceptionClass = Class.forName(description.getType());
+                Constructor<?> constructor = exceptionClass.getConstructor(String.class);
+                Exception exception = (Exception) constructor.newInstance(description.getMessage());
+                throw exception;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public List<AdoptionRequest> getAllAdoptionRequests() {
+        var adoptionRequests = adoptionRequestTarget
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<AdoptionRequest>>() {
+                });
+        return adoptionRequests;
+    }
+
+    public AdoptionRequest addAdoptionRequest(AdoptionRequest adoptionRequest) throws Exception {
+        adoptionRequest.setRequestID(null);  // To make sure the id is not set and avoid bug related to ill-formed UUID on server side
+        var response = adoptionRequestTarget
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(adoptionRequest, MediaType.APPLICATION_JSON));
+
+        if (response.getStatus() == 200 && response.hasEntity()) {
+            var newlyCreatedAdoptionRequest = response.readEntity(AdoptionRequest.class);
+            adoptionRequest.setRequestID(newlyCreatedAdoptionRequest.getRequestID());
+            return newlyCreatedAdoptionRequest;
+        } else {
+            ExceptionDescription description = response.readEntity(ExceptionDescription.class);
+            // TODO Utils class in domain
+            // Utils.buildException(description)
+            try {
+                Class<?> exceptionClass = Class.forName(description.getType());
+                Constructor<?> constructor = exceptionClass.getConstructor(String.class);
+                Exception exception = (Exception) constructor.newInstance(description.getMessage());
+                throw exception;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public boolean deleteAdoptionRequest(UUID requestID) {
+        var response = adoptionRequestTarget
+                .path(requestID.toString())
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+        return response.getStatus() == 200;
+    }
+
+
 }
